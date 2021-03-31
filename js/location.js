1ee1907b0a19d7ea37903830561162000e481bae
marlene32100 <67315304+marlene32100@users.noreply.github.com>
2021-03-31 13:25:25 +0200

    Added erorr function

--- js/location.js
+++ js/location.js
@@ -45,6 +45,23 @@ window.addEventListener("load", () => {
         document.body.style.backgroundImage =
           "url('./assets/images/sunny.jpg')";
       }
+
+      function showError(error) {
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation.";
+            break;
+          case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable.";
+            break;
+          case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out.";
+            break;
+          case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred.";
+            break;
+        }
+      }
     });
   } else {
     alert("Sorry, your browser does not support HTML5 geolocation.");
