a66555591be46261cd7a1e79302db43562f6e4ad
marlene32100 <67315304+marlene32100@users.noreply.github.com>
2021-03-31 13:20:09 +0200

    Fixed js from kelvin to celsius

--- js/location.js
+++ js/location.js
@@ -15,16 +15,19 @@ window.addEventListener("load", () => {
       let api_weather = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`;
       let api_location = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${api_key}`;
 
+      // Fetch data from OpenWeather API
       fetch(api_weather).then((response) => {
         return response.json().then((data) => {
           const temperature = data.current.temp;
-          const celsius = Math.round((temperature - 32 * 5) / 9);
-          const description = data.current.weather[0].main;
+          const celsius = Math.round(temperature - 273.15);
+          const description = data.current.weather[0].description;
           document.getElementById("celsius").innerHTML = celsius;
           document.getElementById("description").innerHTML = description;
+          console.log(data);
         });
       });
 
+      // Get location name from coordinates
       fetch(api_location).then((response) => {
         return response.json().then((data) => {
           const location = data[0].name;
@@ -32,6 +35,7 @@ window.addEventListener("load", () => {
         });
       });
 
+      // Show different image based on daytime/night time
       let time_now = time.split(":")[0];
 
       if (time_now >= 20 || time_now <= 7) {
